8960301dbe02c8421567f2fb64a022edd9d59a1d
Serchacho <83768648+Serchacho@users.noreply.github.com>
2024-01-17 19:23:44 -0500

    Added Extra Steps
    Decided to add the extra challenge steps that were in description. Now program checks to see if first number inputted is a factor of 4 as well as if the first number is a factor of second number inputted

--- Exercise 2 - Odd Or Even/odd_or_even.py
+++ Exercise 2 - Odd Or Even/odd_or_even.py
@@ -11,16 +11,41 @@ Ask the user for two numbers: one number to check (call it num) and one number t
 If not, print a different appropriate message.
 """
 
-print("This program will check to see if the number you input is odd or even.")
-num = int(input("Input a number: "))
+#print("This program will check to see if the number you input is odd or even.")
+print("This program is to see whether the number you input is odd or even, see if it is a multiple of 4, as well as")
+print("to see if your number is a factor of the second number you provide.")
+num1 = int(input("Please input your first number: "))
+num2 = int(input("Please input your second number: "))
 
-# input validation
-#while isinstance(num, (str, float, dict, list, tuple)):
-#    num = input("Sorry, that didn't work. Please input a number: ")
 
-if num % 2 == 0:
-    print("The number " + str(num) + " is even")
-elif num % 2 == 1:
-    print("The number " + str(num) + " is odd")
+
+if num1 % 2 == 0:           #if num1 is even
+    print("The number " + str(num1) + " is even")
+
+    # check if num1 is factor of 4
+    if num1 % 4 == 0:
+        print("And the number " + str(num1) + " is a factor of 4")
+    else:
+        print("And the number " + str(num1) + " is NOT a factor of 4")
+
+    #check if num1 is divisible by num2
+    if num1 % num2 == 0:
+        print("And the number " + str(num1) + " is a factor of " + str(num2) + ".")
+    else:
+        print("And the number " + str(num1) + " is NOT a factor of " + str(num2) + ".")
+elif num1 % 2 == 1:         #if num1 is odd
+    print("The number " + str(num1) + " is odd")
+
+    # check if num1 is factor of 4
+    if num1 % 4 == 0:
+        print("And the number " + str(num1) + " is a factor of 4")
+    else:
+        print("And the number " + str(num1) + " is NOT a factor of 4")
+
+    #check if num1 is divisible by num2
+    if num1 % num2 == 0:
+        print("And the number " + str(num1) + " is a factor of " + str(num2) + ".")
+    else:
+        print("And the number " + str(num1) + " is NOT a factor of " + str(num2) + ".")
 else:
     print("ion know man...")
