7a0026171272118d27b504a20ea9dd56551114cf
Serchacho <83768648+Serchacho@users.noreply.github.com>
2024-01-30 23:21:00 -0500

    Exercise 13 complete
    This exercise was mostly just critical thinking on how to get the fibonacci numbers but it was also practice for functions

--- exercise_13.py
+++ exercise_13.py
@@ -5,13 +5,29 @@ Write a program that asks the user how many Fibonnaci numbers to generate and th
 Take this opportunity to think about how you can use functions. Make sure to ask the user to enter the number of numbers in the sequence to generate.
 (Hint: The Fibonnaci seqence is a sequence of numbers where the next number in the sequence is the sum of the previous two numbers in the sequence. 
 The sequence looks like this: 1, 1, 2, 3, 5, 8, 13, …)
+
 """
 
 def fibonacci(amount):
     num1 = int(0)
     num2 = int(0)
     
-    array = []
+    array = [0] * amount                    # initializes an array varibale with 0 in every cell with the amount specified by user
+    array[0] = 1
+    index = int(1)                          # index = 1 because don't want to touch first cell of array
 
-    for number in range(1, amount):
-        #array.append(number + num1 + num2)
+    #for num in range(1, amount):            # sets all values in array, except first cell, to zero
+    #    array[num] = 0
+
+    while (index < len(array)):           # adds numbers in 1 and 2 cells before current position
+        array[index] = array[index - 1] + array[index - 2]
+        index += 1
+
+    return array
+
+
+length = int(input("How many numbers in the sequence would you like to generate?: "))
+custom = fibonacci(length)                  # custom takes value of array with specified number of fibanacci numbers
+
+print("These are the first " + str(length) + " Fibonacci numbers: " + str(custom))
+        
