1febbeed4b4345ee7fe54999a05a5bd879bea73e
Serchacho <83768648+Serchacho@users.noreply.github.com>
2024-02-01 14:01:29 -0500

    Add files via upload
    Exercise 14 Completed. This exercise was to learn how sets work. I did sets before, so this was more like practice. I created a function that used the same algorithm I used for an earlier exercise to find duplicates, and another function to find duplicates using sets which was WAY easier. Definitely will use them more often.

--- exercise_14.py
+++ exercise_14.py
@@ -9,23 +9,48 @@ Write two different functions to do this - one using a loop and constructing a l
 Go back and do Exercise 5 using sets, and write the solution for that in a different function.
 """
 
-def remove_duplicates(arr1, arr2):
-    custom = []            # intializes custom array
+import random
 
-    notDupe = True
+def random_elements(length, limit):                     # function to randomly place values into array specified by user
+    arr = [random.randint(0, limit + 1) for x in range(0, length)]
+    return arr
 
-    for x in arr1:          # traverses through both arrays
-        for y in arr2:
-            if x==y and notDupe:    # if similar numbers, makes sure it only gets added once
-                custom.append(y)
-                notDupe = False
-            
-        notDupe = True              # once finishes checking, makes sure next number is checked the same
+def remove_duplicates(arr1, arr2):                      # function to remove duplicates using for loops
+
+    custom = [y for x in arr1 for y in arr2 if x==y]    # intializes custom array
+
+    instances = 0
+
+    for x in custom:                                    # traverses through both arrays
+        for y in custom:
+            if x==y:                                    # if similar numbers, makes sure it only gets added once
+                instances += 1
+                if instances > 1:
+                    custom.pop(custom.index(y))         # removes the second instance of a number in custom array
+        instances = 0                                   # once finishes checking, makes sure next number is checked the same
 
     return custom
 
-a = [1,2,3,4,5,6,7,8,9,1]
-b = [1,5,7,11,4,5,1]
+def remove_duplicates_set(arr1, arr2):                  # function to remove duplicates using sets (much easier and less confusing)
+    array1 = set(arr1)
+    array2 = set(arr2)
+    custom = array1.intersection(array2)                #set1.intersection(set2) returns values present in BOTH sets
+    custom = list(custom)                               #converts custom from set into list
+    return custom
 
 
-print(remove_duplicates(remove_duplicates(a,b), b))
+
+length = int(input("Please input how long you want both arrays to be: "))
+limit = int(input("Please input the largest value you want in both arrays: "))
+
+a = random_elements(length, limit)
+b = random_elements(length, limit)
+
+
+# output
+print()
+print("Array a: " + str(a))
+print("Array b: " + str(b))
+print("Array custom: " + str(remove_duplicates(a,b)))
+print("Array set: " + str(remove_duplicates_set(a,b)))
+print()
