40b6fd453ab251c97cdaa989336b7262dfa93b8a
Serchacho <83768648+Serchacho@users.noreply.github.com>
2024-02-06 11:35:47 -0500

    Exercise 16 - Updated
    I was trying to find a way to know which characters the user wants to use. I made a new list and added the characters the user wanted into them if they typed the appropriate number.

--- exercise_16.py
+++ exercise_16.py
@@ -18,7 +18,37 @@ lowerLet = [chr(let) for let in range(97,123)]          # all lower case letters
 upperLet = [chr(let) for let in range(65,90)]           # all upper case letters
 symbols = [chr(i) for i in range(33,48)]                # chr(i) returns character in ASCII at integer i; 33-47 for symbols
 
+# convert all lists into sets
+"""
+numbers = set(numbers)
+lowerLet = set(lowerLet)
+upperLet = set(upperLet)
+symbols = set(symbols)
+"""
+
 print("This program helps to create a unique password for you.")
 include = input("What would you like to include? \n1 - numbers \n2 - lowercase letters \n3 - uppercase letters \n4 - symbols \nPlease include spaces inbetween numbers: ")
 include = include.split()                               # removes the spaces inbetween numbers and creates list of them
+print(include)
+# set of available characters
+available = []
+#available = set(available)
+
+
+
+for num in include:
+    if int(num) == 1:
+        print("Number is 1")
+        available.extend(numbers)
+    if int(num) == 2:
+        print("Number is 2")
+        available.extend(lowerLet)
+    if int(num) == 3:
+        print("Number is 3")
+        available.extend(lowerLet)
+    if int(num) == 4:
+        print("Number is 4")
+        available.extend(lowerLet)
+
+print(available)
 
